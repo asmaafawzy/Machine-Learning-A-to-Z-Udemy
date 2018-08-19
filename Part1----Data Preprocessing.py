90db2367dfc86aa3df975e27e47f62f0e41affcd
Asmaa Fawzy <asmaafawzy319@gmail.com>
2018-08-20 01:04:38 +0200

    Update Part1----Data Preprocessing.py

--- Part1----Data Preprocessing.py
+++ Part1----Data Preprocessing.py
@@ -8,11 +8,13 @@ import numpy as np
 import pandas as pd 
 import matplotlib.pyplot as plt 
 
+""" Load your Datset """
 dataset = pd.read_csv('Data.csv')
 x = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 """ : in the first means all cols and the second means the cols we need only """
 
+""" Solving the problem of the missing values in the dataset """
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values ='NaN' , strategy = 'mean', axis= 0)
 imputer = imputer.fit(x[:, 1:3])
